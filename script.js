faadd89f3ecaa3b500b6fe59c0ea5f52c8daa22b
LuigiImVector <luigi.vector15@virgilio.it>
2021-05-18 17:09:16 +0200

    Finita la pagina dei contatti + Creata la pagina universale + Fixato il problema che non mi consentiva di mostrare solo mezza parola nella sezione 'Presentazioni' + Piccolezze

---
+++ script.js
@@ -0,0 +1,70 @@
+const elems = document.querySelectorAll('.tilt');
+
+const universalTilt = UniversalTilt.init({
+    elements: elems,
+    settings: {
+        exclude: /(iPod|iPhone|iPad|Android)/,
+        shine: false,
+        //reverse: true
+        scale: 1.1,
+        max: 10,
+        perspective: 5000
+    },
+    callbacks: {
+        // callbacks...
+    }
+});
+
+const incremento = 135; // deg + 135 per incrementare la velocità
+var deg = 0;
+
+var progetti = [
+    ["Nome1", "https://www.example.com/", "Descrizione", "img/immagine-forse.png"],
+    ["Nome2", "https://www.example.com/", "Descrizione", "img/immagine-forse.png"],
+    ["Nome3", "https://www.example.com/", "Descrizione", "img/immagine-forse.png"],
+    ["Nome4", "https://www.example.com/", "Descrizione", "img/immagine-forse.png"]
+];
+
+function spin() {
+    var tmp = Math.floor(Math.random() * 10);
+
+    deg += (tmp*incremento)*2;
+
+    document.getElementById("ruota").style.transform = "rotate("+deg+"deg)";
+
+    setTimeout(function() {
+        tmp = Math.floor(Math.random() * progetti.length);
+
+        document.getElementById("name").innerHTML = progetti[tmp][0];
+        document.getElementById("url").href = progetti[tmp][1];
+        document.getElementById("description").innerHTML = progetti[tmp][2];
+
+
+        document.getElementById("box-preview").classList.remove("opacityOff");
+        document.getElementById("box-preview").classList.add("opacityOn");
+    }, 3000); //Transition time ruota
+
+                 
+}
+
+document.getElementById("close").onclick = function () {
+    document.getElementById("box-preview").classList.add("opacityOff");
+    document.getElementById("box-preview").classList.remove("opacityOn");
+}
+
+function marginBottomAuto()
+{
+    altezzaParola = document.getElementById("last-word").offsetHeight;
+
+    altezzaParola = altezzaParola/3;
+
+    document.getElementById("presentazione").style.marginBottom = "-" + altezzaParola + "px";
+}
+
+window.addEventListener('load', function(event) { 
+    marginBottomAuto();
+});
+
+window.addEventListener('resize', function(event) { 
+    marginBottomAuto();
+});
