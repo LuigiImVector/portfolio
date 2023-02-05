8b6245aee0eb85fc6d1819ae65b99018b83a6f3c
ImVector <59611597+LuigiImVector@users.noreply.github.com>
2023-02-05 18:07:06 +0100

    big update: added animated noise (grainy) to background + added noise + added custom cursor + some fixes (mainly in responsive)

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/***** TITLE *****/
+
 const elems = document.querySelectorAll('.tilt');
 
 const universalTilt = UniversalTilt.init({
@@ -15,6 +17,8 @@ const universalTilt = UniversalTilt.init({
     }
 });
 
+/***** WHEEL & CO *****/
+
 const incremento = 135; // deg + 135 per incrementare la velocità
 var deg = 0;
 
@@ -28,7 +32,7 @@ var progetti = [
 function spin() {
     var tmp = Math.floor(Math.random() * 10);
 
-    deg += (tmp*incremento)*2;
+    deg += (tmp*incremento)*3;
 
     document.getElementById("ruota").style.transform = "rotate("+deg+"deg)";
 
@@ -39,21 +43,25 @@ function spin() {
         document.getElementById("url").href = progetti[tmp][1];
         document.getElementById("description").innerHTML = progetti[tmp][2];
 
-
+        document.getElementById("background-opacity").classList.remove("opacityOff");
+        document.getElementById("background-opacity").classList.add("opacityOn");
+        
         document.getElementById("box-preview").classList.remove("opacityOff");
         document.getElementById("box-preview").classList.add("opacityOn");
-    }, 3000); //Transition time ruota
-
-                 
+    }, 3000); //Transition time wheel                 
 }
 
-document.getElementById("close").onclick = function () {
+function opacityOn() {
+    document.getElementById("background-opacity").classList.add("opacityOff");
+    document.getElementById("background-opacity").classList.remove("opacityOn");
+
     document.getElementById("box-preview").classList.add("opacityOff");
     document.getElementById("box-preview").classList.remove("opacityOn");
 }
 
-function marginBottomAuto()
-{
+/***** HALF WORD in the DESCRIPTION *****/
+
+function marginBottomAuto() {
     altezzaParola = document.getElementById("last-word").offsetHeight;
 
     altezzaParola = altezzaParola/3;
@@ -69,14 +77,40 @@ window.addEventListener('resize', function(event) {
     marginBottomAuto();
 });
 
+
+/***** ANIMATION BAR *****/
+
 document.addEventListener('aos:in', ({ detail }) => {
-    Array.from(document.getElementById("container-competenze").querySelectorAll(".progress")).forEach(function (item, index) {
+    Array.from(document.getElementById("animation-competenze").querySelectorAll(".progress")).forEach(function (item, index) {
         item.classList.remove("animationBar");
     })
 });
 
-document.addEventListener('aos:in:container-competenze', ({ detail }) => {
-    Array.from(document.getElementById("container-competenze").querySelectorAll(".progress")).forEach(function (item, index) {
+document.addEventListener('aos:in:animation-competenze', ({ detail }) => {
+    Array.from(document.getElementById("animation-competenze").querySelectorAll(".progress")).forEach(function (item, index) {
         item.classList.add("animationBar");
     })
 });
+
+/***** CUSTOM CURSOR *****/
+
+const hoverThis = document.querySelectorAll('.hover-this');
+const link = document.querySelectorAll('a');
+const cursor = document.querySelector('.cursor');
+
+function animateit (e) {
+    cursor.style.transform = 'translate(-50%, -50%) scale(8)';
+    if (e.type === 'mouseleave') cursor.style.transform = '';
+};
+
+function editCursor (e) {
+    const { clientX: x, clientY: y } = e;
+    cursor.style.left = x + 'px';
+    cursor.style.top = y + 'px';
+};
+
+hoverThis.forEach(b => b.addEventListener('mousemove', animateit));
+hoverThis.forEach(b => b.addEventListener('mouseleave', animateit));
+link.forEach(b => b.addEventListener('mousemove', animateit));
+link.forEach(b => b.addEventListener('mouseleave', animateit));
+window.addEventListener('mousemove', editCursor);
